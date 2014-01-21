18e20d7b96bb831ce359302cbee6d5c414e96c20
Dan Bearl <dan.bearl@gmail.com>
2014-01-21 21:57:40 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,34 @@
+ruby '2.1.0'
+source 'https://rubygems.org'
+
+gem 'bcrypt-ruby'
+gem 'authem'
+gem 'decent_exposure'
+gem 'haml-rails'
+gem 'jbuilder', '~> 1.0.1'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '4.0.2'
+gem 'rdiscount'
+gem 'redcarpet'
+gem 'thin'
+gem 'turbolinks'
+gem 'will_paginate', '~> 3.0'
+
+group :assets do
+  gem 'sass-rails',   '~> 4.0.0'
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'cucumber-rails', '~> 1.4.0'
+  gem 'rspec-rails'
+end
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'fabrication'
+  gem 'database_cleaner'
+  gem 'therubyracer'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ashowofhands::Application.config.secret_key_base = '9ba7c0121e0b728edb70ac1731bbad911267aa76e9404f71f7039e2704168c666e65914352d00fa15baee056376dad18cadf9144646330274e905a11f480968a'
