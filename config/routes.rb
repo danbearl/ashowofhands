ee21e80fe5f71729bd01777f69486dc62cab2a0b
Dan Bearl <dan.bearl@gmail.com>
2014-02-02 17:36:07 -0500

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Ashowofhands::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get 'log_in' => 'user_sessions#new', as: 'log_in'
+  get 'log_out' => 'user_sessions#destroy', as: 'log_out'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :pages
+  resources :users
+
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
