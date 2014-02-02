96b5b0f049923a447ff5752f2808cbe7a45bf681
Dan Bearl <dan.bearl@gmail.com>
2014-02-02 15:44:06 +0000

    Add user model

---
+++ spec/fabricators/user_fabricator.rb
@@ -0,0 +1,7 @@
+Fabricator(:user) do
+  email                "MyString"
+  password_digest      "MyString"
+  remember_token       "MyString"
+  reset_password_token "MyString"
+  session_token        "MyString"
+end
